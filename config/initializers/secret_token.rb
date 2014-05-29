ab714971fc51068754136f32774c38fa6ca3d17f
monsieurpaillard <romain.paillard@gmail.com>
2014-05-29 13:24:01 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gifboard::Application.config.secret_key_base = 'e7a5b76d7dacefe39a82239b0283a113953b2617dac29a3e825b4b56b8f8433c839c2721622b62d870ead064447ce9a5c2cbf44c7680da6a40459107128fc8b6'
